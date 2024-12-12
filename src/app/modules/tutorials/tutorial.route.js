6d264d828e565b95f89056ddbe01f75eaafe0cd5
rhrafiulhaque <rh.rafiul.haque@gmail.com>
2024-12-12 23:49:40 +0600

    added tutorial route updated

--- src/app/modules/tutorials/tutorial.route.js
+++ src/app/modules/tutorials/tutorial.route.js
@@ -8,7 +8,7 @@ const router = Router();
 router.post('/add', [verifyAuth, verifyAdmin], tutorialControllers.tutorialAdd);
 router.patch('/:tutorialId', [verifyAuth, verifyAdmin], tutorialControllers.tutorialUpdate);
 router.delete('/:tutorialId', [verifyAuth, verifyAdmin], tutorialControllers.deleteTutorial);
-router.get('/', verifyAuth, lessonControllers.getAllLesson);
+router.get('/', verifyAuth, tutorialControllers.getAllTutorial);
 
 
 
