503e474c13a9ce31ae21b28a52d9b070d410a376
rhrafiulhaque <rh.rafiul.haque@gmail.com>
2024-12-12 23:15:58 +0600

    lesson router added

---
+++ src/app/modules/lessons/lessons.route.js
@@ -0,0 +1,17 @@
+const Router = require('express');
+const { lessonControllers } = require('./lesson.controller');
+const { verifyAuth, verifyAdmin } = require('../../middlewares/auth');
+
+
+
+const router = Router();
+router.post('/add', [verifyAuth, verifyAdmin], lessonControllers.lessonAdd);
+router.patch('/:lessonId', [verifyAuth, verifyAdmin], lessonControllers.lessonUpdate);
+router.delete('/:lessonId', [verifyAuth, verifyAdmin], lessonControllers.deleteLesson);
+router.get('/', lessonControllers.getAllLesson);
+
+
+
+module.exports.LessonRouter = router
+
+
