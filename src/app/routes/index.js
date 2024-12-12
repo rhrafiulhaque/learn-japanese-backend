071091ddf75d2dbeb9a76762bc7b79c9f51d2087
rhrafiulhaque <rh.rafiul.haque@gmail.com>
2024-12-12 23:48:46 +0600

    added tutorial route updated

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const UserRouter = require('../modules/users/users.route');
 const { LessonRouter } = require('../modules/lessons/lessons.route');
 const { VocabularyRouter } = require('../modules/vocabulary/vocabulary.route');
+const { TutorialRouter } = require('../modules/tutorials/tutorial.route');
 const router = express.Router()
 
 const moduleRoutes = [
@@ -17,6 +18,10 @@ const moduleRoutes = [
     {
         path: '/vocabulary',
         route: VocabularyRouter,
+    },
+    {
+        path: '/tutorial',
+        route: TutorialRouter,
     },
 ]
 moduleRoutes.forEach(route => router.use(route.path, route.route))
